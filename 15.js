8f2ccfdef6450ee7a6b3a22c7ac32ca0d4727aa6
Aleksandr Malikov <schn27@gmail.com>
2020-12-15 19:57:56 +0300

    day 15: rename last with next

--- 15.js
+++ 15.js
@@ -10,15 +10,15 @@ function getLastSpoken(nums, turns) {
 	nums.forEach((num, i) => spoken[num] = i + 1);
 
 	let delta = undefined;
-	let last = 0;
+	let next = undefined;
 
 	for (let turn = nums.length + 1; turn <= turns; ++turn) {
-		last = delta || 0;
-		delta = turn - spoken[last];
-		spoken[last] = turn;
+		next = delta || 0;
+		delta = turn - spoken[next];
+		spoken[next] = turn;
 	}
 
-	return last;
+	return next;
 }
 
 const input = `0,14,6,20,1,4`;
